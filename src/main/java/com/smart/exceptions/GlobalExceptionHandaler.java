96c3d637a90d4753cd2117ca7d10e10b1d044bfa
Debakanta Pradhan <debakantapradhan5@gmail.com>
2024-09-11 01:10:11 +0530

    global exception updated

--- src/main/java/com/smart/exceptions/GlobalExceptionHandaler.java
+++ src/main/java/com/smart/exceptions/GlobalExceptionHandaler.java
@@ -1,8 +1,14 @@
 package com.smart.exceptions;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandaler {
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleGlobaaleException(Exception ex,Model model){
+		return null;
+	}
 }
